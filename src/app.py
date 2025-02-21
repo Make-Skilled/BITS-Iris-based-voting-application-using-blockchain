5814e363b83e68ccf6542aaa74be4b3f199eb7c8
Krishna reddy <chinthaguntakrishnareddy185@gmail.com>
2025-02-21 16:29:30 +0530

    forntend added

--- src/app.py
+++ src/app.py
@@ -6,5 +6,25 @@ app=Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/admin")
+def adminDashboard():
+    return render_template("adminDashboard.html")
+
+@app.route("/addUser")
+def addUser():
+    return render_template("addUser.html")
+
+@app.route("/addPoll")
+def addPoll():
+    return render_template("addPoll.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 if __name__== "__main__":
     app.run(debug=True)
